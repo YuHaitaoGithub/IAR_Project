b2bbcdfb30eaa07bb0637192642dd3657ee0097b
MM-202010052142\Administrator <1424634450@qq.com>
2021-01-31 20:37:57 +0800

    建立了串口通讯和定时器

--- User/main.c
+++ User/main.c
@@ -1,35 +1,13 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  fire
-  * @version V1.0
-  * @date    2015-xx-xx
-  * @brief   基本定时器定时
-  ******************************************************************************
-  * @attention
-  *
-  * 实验平台:野火  STM32 F407 开发板
-  * 论坛    :http://www.firebbs.cn
-  * 淘宝    :https://fire-stm32.taobao.com
-  *
-  ******************************************************************************
-  */
-
-
 #include "stm32f4xx.h"
 #include "bsp_basic_tim.h"
 #include "bsp_led.h"
 #include "bsp_debug_usart.h"
 
 
-uint16_t a = 5000; 
+uint16_t a = 500; 
 uint16_t b = 0; 
 
-/**
-  * @brief  主函数
-  * @param  无
-  * @retval 无
-  */
+
 int main(void) 
 {
 
@@ -38,20 +16,19 @@ int main(void)
  
 	TIMx_Configuration(); 
 	
-	printf("于海涛\n");
 	
  	while(1)
   	{  
 		if(b == 1)
 		{
 			b = 0;
-			a = 10000;
+			a = 1000;
 			TIMx_Configuration();
 		}
 		if(b == 2)
 		{
 			b = 0;
-			a = 5000;
+			a = 500;
 			TIMx_Configuration();
 		}
 			
--- User/tim/bsp_basic_tim.h
+++ User/tim/bsp_basic_tim.h
@@ -12,8 +12,8 @@ extern uint16_t a;
 
 #define BASIC_TIM_IRQn			TIM6_DAC_IRQn
 #define BASIC_TIM_IRQHandler    TIM6_DAC_IRQHandler
-#define Period                  a-1
-#define Prescaler               4200-1
+#define Period                 (a * 10 - 1)
+#define Prescaler             (4200-1)
 
 void TIMx_Configuration(void);
 
