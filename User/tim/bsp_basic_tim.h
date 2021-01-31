d20350b63f61657c2b1aba00c88c9875f02d36c0
MM-202010052142\Administrator <1424634450@qq.com>
2021-01-31 17:35:31 +0800

    新建文件

---
+++ User/tim/bsp_basic_tim.h
@@ -0,0 +1,21 @@
+#ifndef __BASIC_TIM_H
+#define	__BASIC_TIM_H
+
+#include "stm32f4xx.h"
+
+extern uint16_t a;
+
+
+
+#define BASIC_TIM           	TIM6
+#define BASIC_TIM_CLK       	RCC_APB1Periph_TIM6
+
+#define BASIC_TIM_IRQn			TIM6_DAC_IRQn
+#define BASIC_TIM_IRQHandler    TIM6_DAC_IRQHandler
+#define Period                  a-1
+#define Prescaler               4200-1
+
+void TIMx_Configuration(void);
+
+#endif /* __BASIC_TIM_H */
+
